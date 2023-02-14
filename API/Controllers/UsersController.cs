691c11f5ee97063317f8273346b07ac3aa4aebde
TheodosisKats <theodosis.kats@gmail.com>
2023-02-14 22:12:15 +0200

    Configuration
    Add Sqlite
    Migrate DB to update changes
    Create UsersController

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")] // /api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+    }
+}

447ba31385ba027152e84d7169e1022fef42fe37
TheodosisKats <theodosis.kats@gmail.com>
2023-02-21 00:19:42 +0200

    Client Login/Register
    @Client
    Added NavBar w/ Login form
    Added user model
    Added Home Component
    Added Register Component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,21 +10,24 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent {
   model: any = {};
-  loggedIn = false;
 
-  constructor(private accountService: AccountService) { }
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+
+  }
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
     })
   }
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 }

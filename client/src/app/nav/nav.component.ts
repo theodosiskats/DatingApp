5ab50065a5a5627b6d5a99accee36b349419ad07
TheodosisKats <theodosis.kats@gmail.com>
2023-02-21 20:38:51 +0200

    Router Added
    Added Router logic
    Added nav links to router
    Refactor NavComp to redirect on login and logout
    Added Router class as property in ctor @nav.component.ts

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -6,28 +7,24 @@ import { AccountService } from '../_services/account.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
   model: any = {};
 
+  constructor(public accountService: AccountService, private router: Router) {}
 
-  constructor(public accountService: AccountService) { }
-
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: error => console.log(error)
-    })
+      next: () => this.router.navigateByUrl('/members'),
+      error: (error) => console.log(error),
+    });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }

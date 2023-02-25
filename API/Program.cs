8d96112e6c03dc1f6156816a123acf5b9280e8a4
TheodosisKats <theodosis.kats@gmail.com>
2023-02-26 01:33:19 +0200

    @API Seeding DB
    Create Seed Class
    Add UserSeedData
    Add try {seeding} on Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,5 +21,21 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception e)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(e, "An error occurred during db migration");
+    Console.WriteLine(e);
+    throw;
+}
 
 app.Run();

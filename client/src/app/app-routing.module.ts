d9adcf3a00d12a78302c8b12aff97505ab636973
TheodosisKats <theodosis.kats@gmail.com>
2023-02-27 01:45:39 +0200

    @Client Add Edit Profile
    Added Edit Profile UI
    Added Route to Edit Profile

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,36 +1,39 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from './errors/not-found/not-found.component';
-import { ServerErrorComponent } from './errors/server-error/server-error.component';
-import { TestErrorComponent } from './errors/test-error/test-error.component';
-import { HomeComponent } from './home/home.component';
-import { ListsComponent } from './lists/lists.component';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-import { MemberListComponent } from './members/member-list/member-list.component';
-import { MessagesComponent } from './messages/messages.component';
-import { AuthGuard } from './_guards/auth.guard';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {NotFoundComponent} from './errors/not-found/not-found.component';
+import {ServerErrorComponent} from './errors/server-error/server-error.component';
+import {TestErrorComponent} from './errors/test-error/test-error.component';
+import {HomeComponent} from './home/home.component';
+import {ListsComponent} from './lists/lists.component';
+import {MemberDetailComponent} from './members/member-detail/member-detail.component';
+import {MemberListComponent} from './members/member-list/member-list.component';
+import {MessagesComponent} from './messages/messages.component';
+import {AuthGuard} from './_guards/auth.guard';
+import {MemberEditComponent} from "./members/member-edit/member-edit.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  {path: '', component: HomeComponent},
   {
     path: '',
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      { path: 'members', component: MemberListComponent },
-      { path: 'members/:username', component: MemberDetailComponent },
-      { path: 'lists', component: ListsComponent },
-      { path: 'messages', component: MessagesComponent },
+      {path: 'members', component: MemberListComponent},
+      {path: 'members/:username', component: MemberDetailComponent},
+      {path: 'member/edit', component: MemberEditComponent},
+      {path: 'lists', component: ListsComponent},
+      {path: 'messages', component: MessagesComponent},
     ],
   },
-  { path: 'errors', component: TestErrorComponent },
-  { path: 'not-found', component: NotFoundComponent },
-  { path: 'server-error', component: ServerErrorComponent },
-  { path: '**', component: NotFoundComponent },
+  {path: 'errors', component: TestErrorComponent},
+  {path: 'not-found', component: NotFoundComponent},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: '**', component: NotFoundComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}

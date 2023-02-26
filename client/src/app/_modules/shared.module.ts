538acbf35a1d948627b33780e9b0c02391fb623c
TheodosisKats <theodosis.kats@gmail.com>
2023-02-27 00:41:25 +0200

    @Client Create User Details UI
    Added MemberInterface
    Added Environments [for env variables]
    Added Tabset, Gallery Modules
    Changed Routing on member from id to username
    Added Photo Interface
    Added Member Interface
    Added Member Service
    Added Token Interceptor on requests [JWT]
    Added user.png for null IsMain Photo
    Added Members List UI
    Added Member Details UI

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ToastrModule } from 'ngx-toastr';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {ToastrModule} from 'ngx-toastr';
+import {TabsModule} from "ngx-bootstrap/tabs";
+import {NgxGalleryModule} from "@kolkov/ngx-gallery";
 
 @NgModule({
   declarations: [],
@@ -19,9 +21,15 @@ import { ToastrModule } from 'ngx-toastr';
       resetTimeoutOnDuplicate: true,
       includeTitleDuplicates: true,
     }),
+    TabsModule.forRoot(),
+    NgxGalleryModule,
   ],
   exports: [
     BsDropdownModule,
-    ToastrModule],
+    ToastrModule,
+    TabsModule,
+    NgxGalleryModule
+  ],
 })
-export class SharedModule {}
+export class SharedModule {
+}

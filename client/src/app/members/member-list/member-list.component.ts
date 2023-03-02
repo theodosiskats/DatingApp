f5bc0108ea6719223e8191b202417cb888574fd4
TheodosisKats <theodosis.kats@gmail.com>
2023-03-02 22:43:54 +0200

    @API & @Client Add filters
    @API:
    Update UserParams w/ minAge,maxAge filter props
    Update UserRepository to accept query filters for age
    Update UserController to filter genders
    Update UserDto to include Gender prop
    @Client
    Fix path of alternative image for users w/ no profile pics
    Add filter buttons on member list comp to apply filters
    Update Nav username preview to KnownAs to reflect the Dtos returned values
    Refactor/Update Member Service to take extra filter params

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
 import {Member} from "../../_models/member";
 import {MembersService} from "../../_services/members.service";
-import {Observable} from "rxjs";
 import {Pagination} from "../../_models/pagination";
+import {AccountService} from "../../_services/account.service";
+import {UserParams} from "../../_models/userParams";
+import {User} from "../../_models/user";
+import {take} from "rxjs";
 
 @Component({
   selector: 'app-member-list',
@@ -10,13 +13,25 @@ import {Pagination} from "../../_models/pagination";
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent {
-  // members$: Observable<Member[]> | undefined
   members: Member[] = []
   pagination: Pagination | undefined
-  pageNumber = 1;
-  pageSize = 5;
+  userParams: UserParams | undefined
+  user: User | undefined
+  //TODO - Add default value and fetch both man and women
+  genderList = [
+    {value: 'male', display: 'Males'},
+    {value: 'female', display: 'Females'}
+  ]
 
-  constructor(private memberService: MembersService) {
+  constructor(private memberService: MembersService, private accountService: AccountService) {
+      this.accountService.currentUser$.pipe(take(1)).subscribe({
+        next: user => {
+          if (user) {
+            this.userParams = new UserParams(user)
+            this.user = user
+          }
+        }
+      })
   }
 
   ngOnInit(): void {
@@ -24,7 +39,8 @@ export class MemberListComponent {
   }
 
   loadMembers() {
-    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+    if (!this.userParams) return
+    this.memberService.getMembers(this.userParams).subscribe({
       next: response => {
         if (response.result && response.pagination) {
           this.members = response.result;
@@ -34,9 +50,16 @@ export class MemberListComponent {
     })
   }
 
+  resetFilters() {
+    if (this.user) {
+      this.userParams = new UserParams(this.user)
+      this.loadMembers()
+    }
+  }
+
   pageChanges(event: any) {
-    if (this.pageNumber !== event.page) {
-      this.pageNumber = event.page
+    if (this.userParams && this.userParams?.pageNumber !== event.page) {
+      this.userParams.pageNumber = event.page
       this.loadMembers();
     }
   }

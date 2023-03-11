2acdb1cc3099cdd7dfa7e413a9a96ffaebadb6f6
TheodosisKats <theodosis.kats@gmail.com>
2023-03-11 22:23:41 +0200

    API & Client: Likes Component & Pagination
    API:
    Create PaginationParams class
    Create LikesParams
    Add PaginationsParams class to UserParams and LikesParams
    Update Claims GetUserId method to return id instead of string
    Update UserActivity to user int id instead of parsing int on string id
    Client:
    Add content on List Component Template
    Add addLike method on members service
    Add likes logic on List Component
    Add addLike method on hearth button in member card
    Add addLike logic in member card component

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, ViewEncapsulation} from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { MembersService } from 'src/app/_services/members.service';
 import {Member} from "../../_models/member";
 
 @Component({
@@ -8,5 +10,13 @@ import {Member} from "../../_models/member";
 })
 export class MemberCardComponent {
   @Input() member: Member | undefined;
+
+  constructor(private memberService: MembersService, private toastr: ToastrService) {}
+
+  addLike(member: Member){
+    this.memberService.addLike(member.userName).subscribe({
+      next: () => this.toastr.success('You have liked ' + member.knownAs)
+    })
+  }
 
 }

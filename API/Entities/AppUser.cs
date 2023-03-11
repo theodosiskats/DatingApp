929698354cf18e9a29793f244cca8323aeb947e1
TheodosisKats <theodosis.kats@gmail.com>
2023-03-11 20:00:20 +0200

    API: Likes Entity
    Add Likes Entity
    Add Repository & Interface for Likes
    Add Likes Controller
    Add LikeDto
    Updated AppUser Entity
    Create EF Migration for DB

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -29,5 +29,9 @@ public class AppUser
     public string Country { get; set; }
     
     public List<Photo> Photos { get; set; } = new();
-    
+
+    public List<UserLike> LikedByUsers { get; set; }
+
+    public List<UserLike> LikedUsers { get; set; }
+
 }

cafe35146d4ffe25ac053053d9ef626c435dd931
TheodosisKats <theodosis.kats@gmail.com>
2023-03-12 21:07:57 +0200

    Client: Add Send Message
    API [MessageRepository]: Minor change messages order in messages thread response

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewChild} from '@angular/core';
 import {Message} from "../../_models/message";
 import {MessageService} from "../../_services/message.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-member-messages',
@@ -8,12 +9,25 @@ import {MessageService} from "../../_services/message.service";
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent {
+  @ViewChild('messageForm') messageForm?: NgForm
   @Input() username?: string
   @Input() messages: Message[] = []
+  messageContent = ''
 
   constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
   }
 
+  sendMessage() {
+    console.log(this.username)
+    if (!this.username) return;
+    console.log('step2')
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe({
+      next: message => {
+        this.messages.push(message)
+        this.messageForm?.reset()
+      }
+    })
+  }
 }

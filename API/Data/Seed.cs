58d813459f94ddf6e389d4624a2ee3d4f8986c65
TheodosisKats <theodosis.kats@gmail.com>
2023-03-18 14:49:48 +0200

    API: Fly.IO connection string fix

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,6 +1,4 @@
-﻿using System.Security.Cryptography;
-using System.Text;
-using System.Text.Json;
+﻿using System.Text.Json;
 using API.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
--- API/Program.cs
+++ API/Program.cs
@@ -18,11 +18,11 @@ if (builder.Environment.IsDevelopment())
     connString = builder.Configuration.GetConnectionString("DefaultConnection");
 else 
 {
-// Use connection string provided at runtime by FlyIO.
+    // Use connection string provided at runtime by FlyIO.
     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
+ 
     // Parse connection URL to connection string for Npgsql
-    connUrl = connUrl.Replace("postgres://", string.Empty);
+    connUrl = connUrl.Replace(“postgres://“, string.Empty);
     var pgUserPass = connUrl.Split("@")[0];
     var pgHostPortDb = connUrl.Split("@")[1];
     var pgHostPort = pgHostPortDb.Split("/")[0];
@@ -31,8 +31,9 @@ else
     var pgPass = pgUserPass.Split(":")[1];
     var pgHost = pgHostPort.Split(":")[0];
     var pgPort = pgHostPort.Split(":")[1];
-
-    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+    var updatedHost = pgHost.Replace("flycast", "internal");
+ 
+connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
@@ -41,10 +42,9 @@ builder.Services.AddDbContext<DataContext>(opt =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(builder => builder
+app.UseCors(builder => builderfly
     .AllowAnyMethod()
     .AllowAnyHeader()
     .AllowCredentials()

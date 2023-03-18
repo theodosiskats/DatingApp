d5935da8f551aa1874e1b35385bf37fb5324ff87
TheodosisKats <theodosis.kats@gmail.com>
2023-03-18 15:20:39 +0200

    API: Fix typo in Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -22,7 +22,7 @@ else
     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
  
     // Parse connection URL to connection string for Npgsql
-    connUrl = connUrl.Replace(“postgres://“, string.Empty);
+    connUrl = connUrl.Replace("postgres://", string.Empty);
     var pgUserPass = connUrl.Split("@")[0];
     var pgHostPortDb = connUrl.Split("@")[1];
     var pgHostPort = pgHostPortDb.Split("/")[0];
